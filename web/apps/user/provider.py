d39f906bef6dadb66fcaff2a38a90897d37f5dd2
NzKoff <nskvip54@gmail.com>
2021-05-15 01:47:21 +0700

    Полная информация о пользователе

--- web/apps/user/provider.py
+++ web/apps/user/provider.py
@@ -8,5 +8,11 @@ class Provider(BaseProvider):
         super().__init__('user')
 
     def get_user(self, steam_id: str) -> dict:
-        return self.exec_by_file('get_user.tmpl', {'steam_id': steam_id})[0]
+        user = self.exec_by_file('get_user.tmpl', {'steam_id': steam_id})[0]
+        user['reviews_coach'] = self.exec_by_file('get_reviews_coach.tmpl', user)
+        user['reviews_learner'] = self.exec_by_file('get_reviews_learner.tmpl', user)
+        user['learners_coach'] = self.exec_by_file('get_learners_coach.tmpl', user)
+        user['learners_learner'] = self.exec_by_file('get_learners_learner.tmpl', user)
+        user['games_user'] = self.exec_by_file('get_games_user.tmpl', user)
+        return user
 
