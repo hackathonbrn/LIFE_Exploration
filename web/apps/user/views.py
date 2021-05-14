56e4ab2c90e35c3cccf7a73def3f65e3f61e38c3
NzKoff <nskvip54@gmail.com>
2021-05-15 02:36:13 +0700

    Полная информация о пользователе

--- web/apps/user/views.py
+++ web/apps/user/views.py
@@ -1,10 +1,23 @@
-from fastapi import Body, Request, Query, APIRouter
+from fastapi import Body, Request, Query, APIRouter, Response
 from .processor import Processor
 
 router = APIRouter()
 
 
-@router.get("/get_user")
-def get_user(request: Request):
+@router.get("/cabinet")
+def cabinet(request: Request):
     steam_id = request.headers.get('session')
-    return Processor().get_user(steam_id)
+    user_info = Processor().cabinet(steam_id)
+    if user_info:
+        return user_info
+    return Response(status_code=404)
+
+
+@router.get("/get_user")
+def get_user(user_id: int = None):
+    user_info = None
+    if user_id:
+        user_info = Processor().get_user(user_id)
+    if user_info:
+        return user_info
+    return Response(status_code=404)
