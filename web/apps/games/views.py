9e05dfae903ecae8265e75aaa3a81271e493e410
madwayz <djurball@list.ru>
2021-05-15 10:26:49 +0700

    * Added playtime route.

--- web/apps/games/views.py
+++ web/apps/games/views.py
@@ -1,6 +1,7 @@
 from fastapi import Body, Request, Query, APIRouter, Response
 from .processor import Processor
 from .schemas import Games
+from utils.game_list import get_csgo_playtime, get_dota2_playtime
 
 router = APIRouter()
 
@@ -8,3 +9,13 @@ router = APIRouter()
 @router.get("/get_games", response_model=Games)
 def get_games():
     return {'games': Processor().get_games()}
+
+
+@router.get('/get_playtime')
+def get_playtime(game_name: str, user_id: int):
+    if game_name == 'csgo':
+        return get_csgo_playtime(user_id)
+    elif game_name == 'dota2':
+        return get_dota2_playtime(user_id)
+    else:
+        return Response(status_code=400)
--- web/utils/game_list.py
+++ web/utils/game_list.py
@@ -4,33 +4,40 @@ from config import STEAM_WEB_API_KEY
 
 def get_csgo_playtime(id):
     response = requests.get(
-        f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={STEAM_WEB_API_KEY}&steamid={id}&format=json")
+        f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?"
+        f"key={STEAM_WEB_API_KEY}"
+        f"&steamid={id}"
+        f"&format=json")
     response = response.json()
     games = response["response"]["games"]
 
-    response = {}
     for i in games:
         appid = i.get('appid')
         if appid != 730:
             continue
 
         new_time = i.get('playtime_forever') // 60
-        response.update({'appid': appid, 'played_time': new_time})
-    return response
+        data = {'appid': appid, 'played_time': new_time}
+        return data
+
+
 
 
 def get_dota2_playtime(id):
     response = requests.get(
-        f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={STEAM_WEB_API_KEY}&steamid={id}&format=json")
+        f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?"
+        f"key={STEAM_WEB_API_KEY}"
+        f"&steamid={id}"
+        f"&format=json")
     response = response.json()
     games = response["response"]["games"]
 
-    response = {}
     for i in games:
         appid = i.get('appid')
         if appid != 570:
             continue
 
         new_time = i.get('playtime_forever') // 60
-        response.update({'appid': appid, 'played_time': new_time})
-    return response
+        data = {'appid': appid, 'played_time': new_time}
+        return data
+
