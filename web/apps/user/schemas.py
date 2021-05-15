1b9734bc77139124830899d3065c66ea515a00d7
NzKoff <nskvip54@gmail.com>
2021-05-15 21:54:17 +0700

    fix cors

--- web/apps/user/schemas.py
+++ web/apps/user/schemas.py
@@ -10,13 +10,13 @@ class User(BaseModel):
     verification: bool
     avatar_url: str
     steam_id: int
-    games_user: Union[List[Dict[str, Union[int, str]]], None]
-    reviews_learner: Union[List[Dict[str, Union[int, str]]], None]
-    reviews_coach: Union[List[Dict[str, Union[int, str]]], None]
-    learners_learner: Union[List[Dict[str, Union[int, str]]], None]
-    learners_coach: Union[List[Dict[str, Union[int, str]]], None]
-    co_pararms: Union[Dict[str, float], None]
-    le_pararms: Union[Dict[str, float], None]
+    games_user: Union[List[Dict[str, Union[int, str, None]]], None]
+    reviews_learner: Union[List[Dict[str, Union[int, str, None]]], None]
+    reviews_coach: Union[List[Dict[str, Union[int, str, None]]], None]
+    learners_learner: Union[List[Dict[str, Union[int, str, None]]], None]
+    learners_coach: Union[List[Dict[str, Union[int, str, None]]], None]
+    co_pararms: Union[Dict[str, Union[float, None]], None]
+    le_pararms: Union[Dict[str, Union[float, None]], None]
 
 
 class Coach(BaseModel):
