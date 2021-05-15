52a03f80c87ad485d322d0623eb1059f081b219b
madwayz <djurball@list.ru>
2021-05-15 10:17:03 +0700

    * Added playtime for games.

--- web/utils/game_list.py
+++ web/utils/game_list.py
@@ -1,23 +1,36 @@
-import requests as r
-from config import STEAM_WEB_API_KEY as key
+import requests
+from config import STEAM_WEB_API_KEY
 
 
-def game_csgo(id):
-    response = r.get(
-        f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={key}&steamid={id}&format=json")
+def get_csgo_playtime(id):
+    response = requests.get(
+        f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={STEAM_WEB_API_KEY}&steamid={id}&format=json")
     response = response.json()
     games = response["response"]["games"]
-    a = [i for i in games if i.get("appid") == 730]
-    return a
+
+    response = {}
+    for i in games:
+        appid = i.get('appid')
+        if appid != 730:
+            continue
+
+        new_time = i.get('playtime_forever') // 60
+        response.update({'appid': appid, 'played_time': new_time})
+    return response
 
 
-def game_dota2(id):
-    response = r.get(
-        f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={key}&steamid={id}&format=json")
+def get_dota2_playtime(id):
+    response = requests.get(
+        f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={STEAM_WEB_API_KEY}&steamid={id}&format=json")
     response = response.json()
     games = response["response"]["games"]
-    a = [i for i in games if i.get("appid") == 570]
-    return a
 
-#print(game_csgo("76561198225947128"))
-#print(game_dota2("76561198225947128"))
+    response = {}
+    for i in games:
+        appid = i.get('appid')
+        if appid != 570:
+            continue
+
+        new_time = i.get('playtime_forever') // 60
+        response.update({'appid': appid, 'played_time': new_time})
+    return response
