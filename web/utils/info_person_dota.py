61ad0a74829b3cc94e95ad56c0d6821b7ab8c7a7
NzKoff <nskvip54@gmail.com>
2021-05-15 10:00:42 +0700

    Добавлено создание пользователя, если его нет.

--- web/utils/info_person_dota.py
+++ web/utils/info_person_dota.py
@@ -1,9 +1,9 @@
-import requests
+import requests as r
 
 """id ОБЯЗАТЕЛЬНО должен быть steam32"""
 
 
 def info_person_dota(id):
     id = id - 76561197960265728
-    response = requests.get(f"""https://api.opendota.com/api/players/{id}""")
+    response = r.get(f"""https://api.opendota.com/api/players/{id}""")
     return response.json()
--- web/utils/matches.py
+++ web/utils/matches.py
@@ -1,9 +1,9 @@
-import requests
+import requests as r
 
 """id ОБЯЗАТЕЛЬНО должен быть steam32"""
 
 
 def matches(id):
     id = id - 76561197960265728
-    response = requests.get(f"https://api.opendota.com/api/players/{id}/matches")
+    response = r.get(f"https://api.opendota.com/api/players/{id}/matches")
     return response.json()
